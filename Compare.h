29b6a9eceaff58b188ae13e582ff45e8dcd44f91
Hanna Bennett <bennetthanna22@gmail.com>
2016-12-06 13:12:45 -0800

    Remove unnecessary comparator file

--- Compare.h
+++
@@ -1,9 +0,0 @@
-#pragma once
-#include "Patient.h"
-
-struct CompareBySeverity {
-
-    Boolean operator()(const Patient& p1, const Patient& p2) {
-        return p1.severity < right.severity;
-    }
-};
